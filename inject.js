8f8d501e5006f9738584ac072208005d0099d177
Allan T. Harper <ath@caselle.com>
2017-05-23 10:00:47 -0600

    Pull request branches

--- inject.js
+++ inject.js
@@ -41,7 +41,19 @@ function main(dictionary) {
 (function() {
   var dictionary = {};
 
-  $.each(["2017.05", "2017.08", "Development", $(location).attr('href').split('/').pop()], function(__, buildBranch) {
+  var branch = ''
+
+  console.log($("span.css-truncate-target"));
+  if ($(location).attr('href').indexOf("pull") > 0) {
+    branch = $("span.css-truncate-target").last().html();
+  } else {
+    branch = $(location).attr('href').split('/').pop()
+  }
+
+  console.log("Branch");
+  console.log(branch);
+
+  $.each(["2017.05", "2017.08", "Development", branch], function(__, buildBranch) {
     $.each([0, 10, 20, 30, 40], function(_, x) {
       chrome.runtime.sendMessage({
         method: 'GET',

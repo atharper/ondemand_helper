05a5289113107973535b446d00a0924b11281474
Allan Harper <aluhadora@gmail.com>
2017-05-25 17:37:33 -0600

    Adding support for other attask-ondemand domain

--- task_details.js
+++ task_details.js
@@ -1,5 +1,7 @@
 (function () {
-  if ($(location).attr('href').indexOf('caselle.my.workfront.com/task/view') < 0) return;
+  if ($(location).attr('href').indexOf('caselle.my.workfront.com/task/view') < 0 &&
+    $(location).attr('href').indexOf('caselle.attask-ondemand.com/task/view') < 0) return;
+
   var taskLabel = $('.fnt-sm').last();
   var branch = taskLabel.html() + '.11';
 

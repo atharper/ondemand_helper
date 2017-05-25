bae3733cbd174b177dc9d65d6184e8db8f858eb2
Allan T. Harper <ath@caselle.com>
2017-05-24 21:04:50 -0600

    Missed files

---
+++ task_details.js
@@ -0,0 +1,51 @@
+function getColor (build) {
+  if (build.state !== 'Completed') {
+    return 'orange';
+  } else if (build.result === 'Passed') {
+    return 'green';
+  } else if (build.result === 'Failed') {
+    return 'red';
+  }
+  return false;
+}
+
+(function () {
+  if ($(location).attr('href').indexOf('caselle.my.workfront.com/task/view') < 0) return;
+  var taskLabel = $('.fnt-sm').last();
+  var branch = taskLabel.html() + ".11";
+
+  chrome.runtime.sendMessage({
+    method: 'GET',
+    action: 'xhttp',
+    url: 'http://cctray:peekaboo$Treet@gocaselle:8153/go/api/pipelines/' + branch + '/history/0'
+  }, function (responseText) {
+    var response = tryParseJSON(responseText);
+    if (!response) return;
+
+    var pipeline = response.pipelines[0];
+    var build = {
+      label: pipeline.label,
+      state: pipeline.stages[0].jobs[0].state,
+      result: pipeline.stages[0].jobs[0].result,
+      branch: branch,
+      infoLink: 'http://cctray:peekaboo$Treet@gocaselle:8153/go/pipelines/' + branch + '/' + pipeline.label.split('.').pop() + '/MSBuild/1'
+    };
+
+    var color = getColor(build);
+    if (color) taskLabel.css('color', color);
+    taskLabel.text(taskLabel.text() + " ---- " + build.label);
+
+    refreshRun();
+  });
+})();
+
+function tryParseJSON (jsonString) {
+  try {
+    var o = JSON.parse(jsonString);
+    if (o && typeof o === 'object') {
+      return o;
+    }
+  }
+  catch (e) { }
+  return false;
+}

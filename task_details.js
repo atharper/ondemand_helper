574b147d1e5d54d40ff2664211760d7eb05d718d
Allan Harper <aluhadora@gmail.com>
2017-05-25 18:34:06 -0600

    Code Cleanup -- getLastBuild

--- task_details.js
+++ task_details.js
@@ -3,29 +3,13 @@
     $(location).attr('href').indexOf('caselle.attask-ondemand.com/task/view') < 0) return;
 
   var taskLabel = $('.fnt-sm').last();
-  var branch = taskLabel.html() + '.11';
 
-  chrome.runtime.sendMessage({
-    method: 'GET',
-    action: 'xhttp',
-    url: 'http://cctray:peekaboo$Treet@gocaselle:8153/go/api/pipelines/' + branch + '/history/0'
-  }, function (responseText) {
-    var response = tryParseJSON(responseText);
-    if (!response) return;
-
-    var pipeline = response.pipelines[0];
-    var build = {
-      label: pipeline.label,
-      state: pipeline.stages[0].jobs[0].state,
-      result: pipeline.stages[0].jobs[0].result,
-      branch: branch,
-      infoLink: 'http://cctray:peekaboo$Treet@gocaselle:8153/go/pipelines/' + branch + '/' + pipeline.label.split('.').pop() + '/MSBuild/1'
-    };
-
-    var color = getColor(build);
-    if (color) color = 'color: ' + color + ';';
-    else color = '';
-    taskLabel.append('---<span style="font-size: 12px;' + color + '">' + build.label + '</span>');
-    // taskLabel.text(taskLabel.text() + ' ---- ' + build.label);
+  $.each(['.02', '.05', '.08', '.11'], function (_, x) {
+    getLastBuild(taskLabel.html() + x, function(build) {
+      var color = getColor(build);
+      if (color) color = 'color: ' + color + ';';
+      else color = '';
+      taskLabel.append('---<span style="font-size: 12px;' + color + '">' + build.label + '</span>');
+    })
   });
 })();
--- utilities.js
+++ utilities.js
@@ -19,3 +19,54 @@ function getColor (build) {
   }
   return false;
 }
+
+function getLastBuild(branch, funct, page) {
+  page = page || 0;
+
+  chrome.runtime.sendMessage({
+    method: 'GET',
+    action: 'xhttp',
+    url: 'http://cctray:peekaboo$Treet@gocaselle:8153/go/api/pipelines/' + branch + '/history/' + page
+  }, function (responseText) {
+    var response = tryParseJSON(responseText);
+    if (!response) return false;
+
+    var pipeline = response.pipelines[0];
+    funct({
+      label: pipeline.label,
+      state: pipeline.stages[0].jobs[0].state,
+      result: pipeline.stages[0].jobs[0].result,
+      branch: branch,
+      infoLink: 'http://cctray:peekaboo$Treet@gocaselle:8153/go/pipelines/' + branch + '/' + pipeline.label.split('.').pop() + '/MSBuild/1'
+    });
+  });
+}
+
+function getBuilds(funct, branch, page) {
+  page = page || 0;
+
+  chrome.runtime.sendMessage({
+    method: 'GET',
+    action: 'xhttp',
+    url: 'http://cctray:peekaboo$Treet@gocaselle:8153/go/api/pipelines/' + branch + '/history/' + page
+  }, function (responseText) {
+    var response = tryParseJSON(responseText);
+    if (!response) return false;
+
+    $.each(response.pipelines, function(i, pipeline) {
+      $.each(pipeline.build_cause.material_revisions[0].modifications, function(j, modification) {
+        funct()
+
+        if (!dictionary[modification.revision]) dictionary[modification.revision] = {
+          label: pipeline.label,
+          state: pipeline.stages[0].jobs[0].state,
+          result: pipeline.stages[0].jobs[0].result,
+          branch: branch,
+          infoLink: 'http://gocaselle:8153/go/pipelines/' + branch + '/' + pipeline.label.split('.').pop() + '/MSBuild/1'
+        };
+      });
+    });
+
+    refreshRun();
+  });
+}
